fdc22786d47edf162357cebf7912950f9474308c
mohsyhrl <mohamadsyahrul7@gmail.com>
2021-10-28 22:07:41 +0700

    commit

---
+++ assets/js/js.js
@@ -0,0 +1,8 @@
+function on() {
+  var menuBar = document.getElementById("menuBar");
+
+  var overlay = document.getElementById("overlay");
+
+  menuBar.classList.toggle("menuBarStyle");
+  overlay.classList.toggle("overlayStyle");
+}
